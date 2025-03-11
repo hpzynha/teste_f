38e2d7dfbd20ed80106effcb7b9dee3117b8e29c
hpzynha <larissa.nogueira.rocha@gmail.com>
2025-03-11 09:52:23 -0400

    change font

--- lib/design_system/text_style.dart
+++ lib/design_system/text_style.dart
@@ -3,12 +3,12 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:test_f/design_system/colors.dart';
 
 class AppTextStyle {
-  static TextStyle productTitle = GoogleFonts.lato(
+  static TextStyle productTitle = GoogleFonts.montserrat(
     fontSize: 24,
     fontWeight: FontWeight.w700,
     color: AppColors.textColor,
   );
-  static TextStyle price = GoogleFonts.lato(
+  static TextStyle price = GoogleFonts.montserrat(
     fontSize: 34,
     fontWeight: FontWeight.w700,
     color: AppColors.textColor,
